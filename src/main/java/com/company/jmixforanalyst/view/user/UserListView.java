18e7bea2109ec2cd3a576b23775e393ca93af5f2
Stas Makarov <s.makarov@haulmont.com>
2025-02-07 14:47:57 +0300

    Initial commit

---
+++ src/main/java/com/company/jmixforanalyst/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.jmixforanalyst.view.user;
+
+import com.company.jmixforanalyst.entity.User;
+import com.company.jmixforanalyst.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "jal_User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
